5f848e6f4ecb406376b932aad5e4b0b1fe749035
Lucas Ferrari <lucasferrarisoares@Gmail.com>
2025-05-23 17:54:55 -0300

    Finalizando projeto

--- Saque Pelo terminal/src/Main.java
+++ Saque Pelo terminal/src/Main.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     static int[] notas = {200, 100, 50, 20, 10, 5, 2};
+    static int[] estoqueNotas = {2, 2, 3, 0, 1, 10, 0};
+
     static int[] moedas = {100, 50, 25, 10, 5, 1};
+    static int[] estoqueMoedas = {10, 10, 10, 10, 10, 10};
+
+    public static int sobra;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,18 +18,14 @@ public class Main {
         System.out.println("Qual valor deseja sacar?");
         Float valorSaque = sc.nextFloat();
 
-        int valorNotas = valorSaque.intValue();
-        int valorCentavos = (int) ((valorSaque - valorNotas) * 100);
-
-        if (temMoedaUm(valorNotas)) {
-            valorCentavos += 100;
-        }
-
         System.out.println("\nQuantidade a ser sacada\n");
-        List<Integer> resultadoNotas = quantidadeTroco(valorNotas, notas);
-        devolveQuantidade(resultadoNotas, " nota(s) de ", notas);
 
-        List<Integer> resultadoMoedas = quantidadeTroco(valorCentavos, moedas);
+        int valorNotas = valorSaque.intValue();
+        List<Integer> resultadoNotas = quantidadeTroco(valorNotas, notas, estoqueNotas);
+        int valorCentavos = (int) ((valorSaque - valorNotas + sobra) * 100);
+        List<Integer> resultadoMoedas = quantidadeTroco(valorCentavos, moedas, estoqueMoedas);
+
+        devolveQuantidade(resultadoNotas, " nota(s) de ", notas);
         devolveQuantidade(resultadoMoedas, " moeda(s) de ", moedas);
     }
 
@@ -41,25 +42,27 @@ public class Main {
 
 
     //Vai dividindo o valor total moedas/notas pelas existentes no caixa, ao final,
-    //retorna a quantidade necessária de cada moedas/notas.
-    public static List<Integer> quantidadeTroco(int valor, int[] notas) {
+    //retorna a quantidade necessária de cada moedas/notas
+    public static List<Integer> quantidadeTroco(int valor, int[] valores, int[] estoque) {
         List<Integer> resultado = new ArrayList<>();
 
-        for (int nota : notas) {
-            int qtd = valor / nota;
-            resultado.add(qtd);
-            valor %= nota;
+        for (int i = 0; i < valores.length; i++) {
+            int qtdUsada = Math.min(valor / valores[i], estoque[i]);
+            resultado.add(qtdUsada);
+            valor -= qtdUsada * valores[i];
+        }
+        if(!(verificaMoeda(valores)) && valor > 0) {
+            sobra = valor;
+        } if (valor > 1) {
+            System.out.println("\nEstoque de notas/moedas não suporta o valor solicitado\n");
+            System.exit(0);
         }
         return resultado;
     }
 
-    //Verifica se o saque necessita de moeda de 1 real.
-    public static boolean temMoedaUm(int quantidade) {
-        for (int nota : notas) {
-            int qtd = quantidade / nota;
-            quantidade %= nota;
-        }
-        if (quantidade == 1) {
+    //verificar se a lista passada é a lista de moedas
+    public static Boolean verificaMoeda(int[] lista) {
+        if (lista == moedas) {
             return true;
         } else {
             return false;
